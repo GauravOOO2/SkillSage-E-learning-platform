c120423b959b4daafae58bfcc9a214931430b5e9
Gaurav0002 <gauravking918@gmail.com>
2024-05-10 21:39:46 +0530

    Styles fix

--- src/components/Tabs.js
+++ src/components/Tabs.js
@@ -4,8 +4,8 @@ import homeIcon from '../assets/home-removebg-preview.png'
 
 const Tabs = () => {
   return (
-    <div className='  flex mx-4 sm:mx-8 md:mx-12 lg:mx-16 my-8 sm:my-12'>
-      <div className='flex items-center'>
+    <div className=' flex flex-col sm:flex-row md:flex-row  mx-4 sm:mx-8 md:mx-12 lg:mx-16 my-8 sm:my-12'>
+      
         <div className='cursor-pointer h-5 w-5 sm:h-6 sm:w-6 top-1 left-1.5'>
           <img src={homeIcon} alt='home' />
         </div>
@@ -15,7 +15,7 @@ const Tabs = () => {
         <p className='hover:text-[#06286E] hover:underline cursor-pointer	text-0 sm:text-[15px] md:text-[18px] text-gray-500 leading-5 content-center font-inter font-normal'>
           Flashcard
         </p>
-      </div>
+     
       <div className='w-4 sm:w-6 pt-3.5 sm:pt-0.5 mg:pt-0.5'>
         <img src={arrowIcon} alt='arrow' />
       </div>
